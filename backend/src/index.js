f4a196737ccc8950e7e74e9fface0bf5f5045120
Manish Mapakshi <manishmapakshi96@gmail.com>
2024-10-12 19:59:11 -0400

    create greeting api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,6 @@
+//Importing utils file
+const { getTimeOfTheDay, constructGreeting } = require("./utils/utils");
+
 // Importing express
 const express = require("express");
 
@@ -7,7 +10,15 @@ const app = express();
 // Setting up a port
 const port = 3001;
 
+// Setting up a route
+app.get("/hello", (req, res) => {
+  const name = req.query?.name || "Stranger";
+  const timeOfTheDay = getTimeOfTheDay();
+  const greeting = constructGreeting(name, timeOfTheDay);
+  res.json({ message: greeting });
+});
+
 // Running the server
 app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
