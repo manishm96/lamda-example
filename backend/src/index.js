3a65582e66d3d241a542da6b1c99c5f24cf3a39d
Manish Mapakshi <manishmapakshi96@gmail.com>
2024-10-12 19:06:40 -0400

    setting up a backend server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+// Importing express
+const express = require("express");
+
+// Creating an express instance
+const app = express();
+
+// Setting up a port
+const port = 3001;
+
+// Running the server
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
